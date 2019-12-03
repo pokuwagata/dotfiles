adaca08e15ae0d2b2f4ca11862b4eff5f52ef75b
pokuwagata <pokuwagata@gmail.com>
2019-12-03 19:16:03 +0900

    fix for workplace

--- .vim/init/keymap.vim
+++ .vim/init/keymap.vim
@@ -1,1 +1,2 @@
 map <C-b> :NERDTreeToggle<CR>
+let mapleader = "\<Space>"
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 abspath=$(pwd)
 ln -sfi $abspath/.vimrc ~/.vimrc
-ln -sfi $abspath/.vim ~/.vim
+cp -rs $abspath/.vim ~/.vim
 ln -sfi $abspath/.zshrc ~/.zshrc
 ln -sfi $abspath/.zshenv ~/.zshenv
 ln -sfi $abspath/.tmux.conf ~/.tmux.conf
