2164fcf743ce80a081b2f6df9cc2c6812f930efc
pokuwagata <pokuwagata@gmail.com>
2023-05-01 11:33:02 +0900

    .gitconfig は環境毎に設定するのでリンクしない

--- symlink.sh
+++ symlink.sh
@@ -6,6 +6,5 @@ ln -sfi $abspath/.vim ~/.vim
 ln -sfi $abspath/.zshrc ~/.zshrc
 ln -sfi $abspath/.zshenv ~/.zshenv
 ln -sfi $abspath/.tmux.conf ~/.tmux.conf
-ln -sfi $abspath/.gitconfig ~/.gitconfig
 
 echo 'done'
