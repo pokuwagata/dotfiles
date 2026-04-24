3623ab9e45855d2af07e8887743fbfeff556b784
pokuwagata <pokuwagata@gmail.com>
2026-04-24 13:39:51 +0900

    symlink 更新

--- symlink.sh
+++ symlink.sh
@@ -7,6 +7,7 @@ ln -sfi $abspath/.zshrc ~/.zshrc
 ln -sfi $abspath/.zshenv ~/.zshenv
 ln -sfi $abspath/.config/sheldon/plugins.toml ~/.config/sheldon/plugins.toml
 ln -sfi $abspath/.config/ghostty/config.ghostty ~/.config/ghostty/config.ghostty
-ln -sfi $abspath/.config/yazi ~/.config/yazi/
+ln -sfi $abspath/.config/yazi ~/.config
+ln -sfi $abspath/.config/nvim ~/.config
 
 echo 'done'
